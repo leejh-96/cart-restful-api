cdc15a7c3df20883ad46d4b7c5e62ed7a6b2ba95
leejh-96 <sbxl12@naver.com>
2023-12-21 21:46:15 +0900

    [feat] EntityModel에 주입할 응답 객체인 ResponseDTO 클래스 생성

---
+++ restful-service/src/main/java/com/outliercart/restfulservice/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.outliercart.restfulservice.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class ResponseDTO {
+
+    private String information;
+
+    public ResponseDTO(String information) {
+        this.information = information;
+    }
+}
