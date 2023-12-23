db9b9336ce7e32092d3e1eadf05d098bb7517049
leejh-96 <sbxl12@naver.com>
2023-12-23 20:46:55 +0900

    [refactor] 기존에 save메서드명에서 createdUsers로 메서드명 변경, 클래스 내부 private 메서드였던 duplicatedIdCheck,duplicatedEmailCheck 삭제 후 createdUsers 내부에 로직으로 추가

--- restful-service/src/main/java/com/outliercart/restfulservice/service/UsersRegisterService.java
+++ restful-service/src/main/java/com/outliercart/restfulservice/service/UsersRegisterService.java
@@ -3,10 +3,8 @@ package com.outliercart.restfulservice.service;
 import com.outliercart.restfulservice.dto.UsersRegisterDTO;
 import com.outliercart.restfulservice.exception.UsersRegisterException;
 import com.outliercart.restfulservice.repository.UsersRegisterDao;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class UsersRegisterService {
     
@@ -19,29 +17,22 @@ public class UsersRegisterService {
         this.usersRegisterDao = usersRegisterDao;
     }
 
-    public UsersRegisterDTO save(UsersRegisterDTO usersRegisterDTO) {
+    public UsersRegisterDTO createdUsers(UsersRegisterDTO usersRegisterDTO) {
 
         //사용중인 ID 인지, 사용중인 EMAIL 인지 체크.
-        duplicateIdCheck(usersRegisterDTO.getUserId());
-        duplicateEmailCheck(usersRegisterDTO.getUserEmail());
+        if (usersRegisterDao.findById(usersRegisterDTO.getUserId()) != 0)
+            throw new UsersRegisterException("이미 사용중인 아이디입니다.");
+
+        if (usersRegisterDao.findByEmail(usersRegisterDTO.getUserEmail()) != 0)
+            throw new UsersRegisterException("이미 사용중인 이메일입니다.");
 
         //jasypt 라이브러리를 사용하여 비밀번호 암호화.
         String encryptPassword = passwordService.encryptPassword(usersRegisterDTO.getUserPassword());
         usersRegisterDTO.setUserPassword(encryptPassword);
 
         //DB 저장.
-        usersRegisterDao.save(usersRegisterDTO);
+        usersRegisterDao.createdUsers(usersRegisterDTO);
         return usersRegisterDTO;
-    }
-
-    private void duplicateIdCheck(String userId){
-        if (usersRegisterDao.findById(userId) != 0)
-            throw new UsersRegisterException("이미 사용중인 아이디입니다.");
-    }
-
-    private void duplicateEmailCheck(String userEmail){
-        if (usersRegisterDao.findByEmail(userEmail) != 0)
-            throw new UsersRegisterException("이미 사용중인 이메일입니다.");
     }
 
 }
