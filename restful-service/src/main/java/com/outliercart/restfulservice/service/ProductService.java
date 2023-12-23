7e5552ac3961c3108971cc26539465d104f0543e
leejh-96 <sbxl12@naver.com>
2023-12-23 20:57:03 +0900

    [refactor] 기존 메서드명이었던 save를 createdProducts로 변경, 기존 메서드명이었던 singleProductsPosts에서 selectedProducts로 변경, pageExistsCheck,postExistsCheck 메서드 삭제후 해당 로직을 selectedProducts메서드에 추가

--- restful-service/src/main/java/com/outliercart/restfulservice/service/ProductService.java
+++ restful-service/src/main/java/com/outliercart/restfulservice/service/ProductService.java
@@ -1,16 +1,13 @@
 package com.outliercart.restfulservice.service;
 
 import com.outliercart.restfulservice.commons.PageInfo;
-import com.outliercart.restfulservice.dto.ProductsRegisterDTO;
-import com.outliercart.restfulservice.exception.PageNotFoundException;
+import com.outliercart.restfulservice.dto.ProductsDTO;
 import com.outliercart.restfulservice.exception.ProductNotFoundException;
 import com.outliercart.restfulservice.repository.ProductsDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -22,35 +19,30 @@ public class ProductService {
         this.productsDao = productsDao;
     }
 
-    public ProductsRegisterDTO save(ProductsRegisterDTO productsRegisterDTO) {
-        productsDao.save(productsRegisterDTO);
-        return productsRegisterDTO;
+    public ProductsDTO createdProducts(ProductsDTO productsDTO) {
+        productsDao.createdProducts(productsDTO);
+        return productsDTO;
     }
 
     public int allProductsCount(PageInfo pageInfo) {
+
         return productsDao.allProductsCount(pageInfo);
+
     }
 
-    public List<ProductsRegisterDTO> allProductsList(PageInfo pageInfo){
+    public List<ProductsDTO> allProductsList(PageInfo pageInfo){
         return productsDao.allProductsPosts(pageInfo);
     }
 
-    private void pageExistsCheck(PageInfo pageInfo) {
-        if (pageInfo.getPage() > pageInfo.getEndPage())
-            throw new PageNotFoundException("존재하지 않는 페이지 입니다.");
-    }
+    public ProductsDTO selectedProducts(int productNo) {
 
-    public ProductsRegisterDTO singleProductsPosts(int productNo) {
-        ProductsRegisterDTO singleProductsPosts = productsDao.singleProductsPosts(productNo);
+        ProductsDTO selectedProducts = productsDao.selectedProducts(productNo);
 
         //게시물이 존재하는지 확인
-        postExistsCheck(singleProductsPosts);
-
-        return singleProductsPosts;
-    }
-
-    private void postExistsCheck(ProductsRegisterDTO singleProductsPosts){
-        if (singleProductsPosts == null)
+        if (selectedProducts == null)
             throw new ProductNotFoundException("존재하지 않는 상품 입니다.");
+
+        return selectedProducts;
     }
+
 }
